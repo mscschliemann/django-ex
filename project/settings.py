bc4839d6a2aaf17d7e0a371099956880aaf6ec90
mschliemann <mschliemann@outlook.de>
2024-05-05 00:27:48 +0000

    aaa

--- project/settings.py
+++ project/settings.py
@@ -140,3 +140,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 INTERNAL_IPS = ['127.0.0.1']
+
+ALLOWED_HOSTS = ['172.30.160.34']
